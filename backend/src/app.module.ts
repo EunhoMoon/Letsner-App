6526f499d46fb80e9bdc07941e847b50b2159335
Janek <eunho.moon@gmail.com>
2023-05-07 21:43:45 +0900

    config, user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(5000),
+        SECRET_KEY: Joi.string().required(),
+        ADMIN_USER: Joi.string().required(),
+        ADMIN_PASSWORD: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Application {
+  private logger = new Logger(Application.name);
+  private readonly DEV_MODE: boolean;
+  private readonly PORT: string;
+  // private corsOriginList: string[];
+
+  constructor(private server: NestExpressApplication) {
+    this.server = server;
+
+    this.DEV_MODE = process.env.NODE_ENV !== "production";
+    this.PORT = process.env.PORT;
+    console.log(process.env.PORT);
+    process.env
+  }
+
+  private async setUpGlobalMiddleware() {
+
+  }
+
+  async bootstrap() {
+    await this.setUpGlobalMiddleware();
+    await this.server.listen(this.PORT);
+  }
+
+  startLog() {
+    if (this.DEV_MODE) {
+      this.logger.log(`✅ Server on http://localhost:${this.PORT}`)
+    } else {
+      this.logger.log(`✅ Server on port ${this.PORT}...`)
+    }
+  }
+
+  errorLog(error: string) {
+    this.logger.error(`🆘 Server error ${error}`)
+  }
 }
-bootstrap();
+
+async function init(): Promise<void> {
+  const server = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = new Application(server);
+  await app.bootstrap();
+  app.startLog();
+}
+
+init().catch((error) => {
+  new Logger("init").error(error);
+});
+
