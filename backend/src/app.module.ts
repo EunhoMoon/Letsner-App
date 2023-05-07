3b6979f185bd39330e4cd25a96ebd4371845f2cb
Janek <eunho.moon@gmail.com>
2023-05-07 23:12:23 +0900

    domain 분리

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UserModule } from "./user/user.module";
+import { UserModule } from "./domain/user/user.module";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmConfig } from "./config/typeormConfig";
